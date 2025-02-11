847431e5ec1ee95cbbeefaf5e3ccf35b79ed60e2
kokach1 <jeswinjoy3695@gmail.com>
2025-02-11 20:17:03 +0530

    Auto commit ‡8 of project ‡js

---
+++ 4.js
@@ -0,0 +1,6 @@
+let mark =[2,4,512,6,5,5,86,];
+console.log(mark);// op will be the array along with its size , which can be expanted by cleaking the arrow it will then include elements by their index also length 
+//length of the array also be printed by 
+console.log(mark.length);//length property of an array 
+let chararray=["aiurghl","8weuhoyrg","rugihe"];
+console.log(chararray);
