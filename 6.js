9b09e4a1563378f5be7416114200694ffadb1c05
kokach1 <jeswinjoy3695@gmail.com>
2025-08-29 12:13:51 +0530

    Auto commit ‡16 of project ‡js

---
+++ 6.js
@@ -0,0 +1,13 @@
+ // window object : it represeents an open window in a browser , it s not an object of js and is automatically crated by the brouser
+ 
+ // DOM: document object model (every element in html is converted in objects [which is called document]in js enabling us to access it in js)
+ console.log(window);// prints out window to the console 
+ console.log(window.document);//prints out documet (ie the html file in console)in the console
+ console.dir(window.document);// dir is used to print properties and methodes of special object (eg:document)
+
+ //its used to make the page dinamic
+
+ let k= document.getElementById("lol");
+ console.log(k);//this prints out whole syntax with that id 
+ console.dir(k); 
+
